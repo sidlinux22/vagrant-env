e53b21a2cfdd52119b56fd934dec1fa41cd34c0e
sidlinux22 <siddhartsharma@gmail.com>
2013-06-10 06:01:12 +0530

    Update config.pp

--- manifests/classes/config.pp
+++ manifests/classes/config.pp
@@ -1,7 +1,7 @@
-# replacefile::config
+# efile::config
 #
-# This resource is an example of what you can do with replacefile.
-# replacefile::config manages simple configuration files which follow the scheme
+# This resource is an example of what you can do with efile.
+# efile::config manages simple configuration files which follow the scheme
 # PARAMETER = VALUE
 # A special feature is, that existing entries which are deactived with the pound sign, are reused.
 # E.g. >># PARAMETER = DEFAULT VALUE<< will we reused, but ONLY if there is no other line like >>PARAMETER = ANYTHING<<.
@@ -12,7 +12,7 @@
 # @param[String]  $ensure : set the configuration entry to this value, or use 'absent' to remove the entry $entry
 # @param[String]  $sep    : the separator to use, e.g. set to ' = '
 # @param[Boolean] $quote  : shall the value be quoted, like >> ENTRY = "value" <<
-define replacefile::config( $path, $entry = false, $ensure, $sep = '=', $quote = false, $no_fail_without_parent = false ) {
+define efile::config( $path, $entry = false, $ensure, $sep = '=', $quote = false, $no_fail_without_parent = false ) {
 
   if $entry == false {
 
@@ -20,7 +20,7 @@ define replacefile::config( $path, $entry = false, $ensure, $sep = '=', $quote =
       path => $path,
       line => $ensure,
     }
-    
+
   } else {
 
     if $quote == true {
@@ -28,32 +28,32 @@ define replacefile::config( $path, $entry = false, $ensure, $sep = '=', $quote =
     } else {
       $_ensure = "${entry}${sep}${ensure}"
     }
-    
+
     Editfile {
       path   => $path,
     }
 
     if $ensure == absent {
-      replacefile { "$title":
+      efile { "$title":
         # we remove all matching entries, but not the comment lines
         match  => "/^${entry}${sep}/",
         ensure => absent,
       }
     } else {
-      
-      replacefile { "${title}":
+
+      efile { "${title}":
         # either match the with leading pound sign AND no other entry without pound - to replace the deactivated entry
         # OR
         # match simply without the pound sign - to replace an active entry
         # OR
-        # if there is no match, replacefile automatically appends our entry at EOF
+        # if there is no match, efile automatically appends our entry at EOF
         match  => "/^(#?\s*${entry}\s?(?!.*^${entry})|${entry}\s?)/m",
         ensure => $_ensure,
         no_fail_without_parent => $no_fail_without_parent,
       }
-      
+
     }
 
   }
-  
+
 }
